7793520f1ec613294950161aa55e5294dfba996f
Andressa Collin <dressaco@outlook.com>
2021-05-05 05:01:32 +0000

    update 1013 using max()

--- 1013.py
+++ 1013.py
@@ -3,7 +3,8 @@ a = int(a)
 b = int(b)
 c = int(c)
 
-maior = (a+b+abs(a-b))/2
-maior = (maior+c+abs(maior-c))/2
+#maior = (a+b+abs(a-b))/2
+#maior = (maior+c+abs(maior-c))/2
+#print (f'{int(maior)} eh o maior')
 
-print (f'{int(maior)} eh o maior')
+print (f'{max(a,b,c)} eh o maior')
